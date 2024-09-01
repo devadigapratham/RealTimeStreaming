aa3fce4e3f89ea07fb24de4c1abd500b4947c520
devadigapratham <devadigapratham8@gmail.com>
2024-09-01 19:16:09 +0530

    initial

---
+++ src/main/java/Dto/SalesPerCategory.java
@@ -0,0 +1,14 @@
+package Dto;
+
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SalesPerCategory {
+
+    private Date transactionDate;
+    private String category;
+    private Double totalSales;
+}
